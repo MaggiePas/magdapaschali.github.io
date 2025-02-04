dd284d3f61c0df2c3ce031de6536431aea6703de
Magda Paschali <paschali@stanford.edu>
2025-02-04 09:19:41 -0800

    Updated website

---
+++ education-timeline.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const EducationTimeline = () => {
+  return (
+    <div className="education-timeline">
+      <div className="timeline-item">
+        <div className="timeline-date">2017</div>
+        <div className="timeline-content">
+          <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+        </div>
+      </div>
+
+      <div className="timeline-item">
+        <div className="timeline-date">2016</div>
+        <div className="timeline-content">
+          <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci.</p>
+        </div>
+      </div>
+
+      <div className="timeline-item">
+        <div className="timeline-date">2015</div>
+        <div className="timeline-content">
+          <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto perfecto perfecti perfect!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EducationTimeline;
